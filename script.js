9f948cbeed25410338172f5dd8a03241d3e003e5
SamuelFletcher7 <samuelfletcher07@gmail.com>
2022-10-28 15:33:57 +0100

    adding cards to binder

--- script.js
+++ script.js
@@ -144,39 +144,53 @@ function findBook (isbn) {
 // Iterate over bookArray and find the target and return it
 
 for (let i = 0; i <bookArray.length; i++) {
-if (isbn === bookArray[i].isbn) {
-
-
-  return bookArray[i]
-}
-
-}
-
-
+  if (isbn === bookArray[i].isbn) {
+    return bookArray[i]
+    }
+  }
 }
 
 function createCard (targetBook) {
-let binder = document.querySelector(".binder")
-let p1 = document.createElement("p")
-p1.innerText = targetBook.subjects
-binder.appendChild(p1)
+  let binder = document.querySelector(".binder")
+  let div1 = document.createElement("div")
+  div1.classList.add("binderContents")
+  let h2 = document.createElement("h2")
+  let div2 = document.createElement("div")
+  div2.classList.add("binderContentsMain")
+  let p1 = document.createElement("p")
+  let p2 = document.createElement("p")
+  let p3 = document.createElement("p")
+  p3.classList.add("subjects")
+  let p4 = document.createElement("p")
+
+  h2.innerText = targetBook.title
+  p1.innerText = targetBook.author
+  p2.innerText = "Published " + targetBook.firstPublishYear
+  if(targetBook.subjects){
+    p3.innerText = `${targetBook.subjects[0]}, ${targetBook.subjects[1]}, ${targetBook.subjects[2]}`
+  }
+  if(targetBook.firstSentence){
+    p4.innerHTML = `<em>${targetBook.firstSentence[0]}</em>`
+  } 
+  
+  binder.appendChild(div1)
+  div1.appendChild(h2)
+  div1.appendChild(div2)
+  div2.appendChild(p1)
+  div2.appendChild(p2)
+  div2.appendChild(p3)
+  div2.appendChild(p4)
 }
 
 function addEvents () {
-
   const cardBtn = document.getElementsByClassName("card-btn")
   const cardArray = Array.from(cardBtn)
   cardArray.forEach((target) => {
-
-target.addEventListener ("click", () => {
-  console.log(target.id)
-const targetBook  = findBook(target.id)
-  createCard(targetBook)
-  
-})
-
+    target.addEventListener ("click", () => {
+      const targetBook  = findBook(target.id)
+    createCard(targetBook)
+    })
   })
-    
 }
 
 
